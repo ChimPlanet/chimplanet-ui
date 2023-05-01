8e3b0da330f8058bbda272ec6f5733d2744ad9bd
byungjin <byungjin.dev@gmail.com>
2023-05-01 21:20:41 +0900

    feat: Add Fallback

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -1,5 +1,21 @@
+import { styled } from "@/libs";
+
 export function Fallback() {
-  return <></>;
+  return (
+    <Container>
+      <Content>데이터를 불러오는 것에 실패하였습니다.</Content>
+    </Container>
+  );
 }
 
 export default Fallback;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Content = styled.span`
+  display: inline-block;
+  margin: auto;
+`;
