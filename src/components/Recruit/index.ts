3b70a5c393d894a0f3f97d2356e3eaa7dc00ce95
byungjin <byungjin.dev@gmail.com>
2023-08-12 18:13:57 +0900

    fix: Fix Recruit Export Statement

--- src/components/Recruit/index.ts
+++ src/components/Recruit/index.ts
@@ -1,12 +1,14 @@
-export * from "./RecruitContext";
-export * from "./RecruitItemContext";
-
 import { RecruitHeader } from "./RecruitHeader";
 import { RecruitItem, RecruitItems } from "./RecruitItems";
 import { RecruitProvider } from "./RecruitProvider";
 
-export default Object.assign(RecruitProvider, {
+export * from "./RecruitContext";
+export * from "./RecruitItemContext";
+
+const Recruit = Object.assign(RecruitProvider, {
   Header: RecruitHeader,
   Items: RecruitItems,
   Item: RecruitItem,
 });
+
+export { Recruit };
